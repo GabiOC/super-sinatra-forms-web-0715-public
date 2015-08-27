b36c5f5998ec74e8ab80a857faa77b53b955c110
GabiOC <oconnor.gabriele@gmail.com>
2015-08-27 10:03:59 -0400

    all done

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :root, "#{settings.root}/."
 
+  get '/' do
+    erb :'super_hero'
+  end
+
+  post '/teams' do
+    @team = params[:team]
+    erb :'team'
+  end
 end
